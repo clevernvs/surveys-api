99bfab859ed7c9560276ccdeaffa290e9090f538
Cleverton Neves <clever.nvs@gmail.com>
2025-06-30 17:53:46 -0300

    refact: Refatorando a service de Questionarios

--- src/services/questionnaire.service.ts
+++ src/services/questionnaire.service.ts
@@ -1,9 +1,16 @@
 export class QuestionnaireService {
     async findAll() {
         return [
-            { id: 1, title: 'Titulo do Questionario 01', project_id: 1, type: 'Redes Sociais', status: 'ACTIVE', created_at: '2025-06-01', updated_at: '2025-08-01' },
-            { id: 2, title: 'Titulo do Questionario 02', project_id: 2, type: 'Comunidades', status: 'ACTIVE', created_at: '2025-06-01', updated_at: '2025-08-01' },
-            { id: 3, title: 'Titulo do Questionario 03', project_id: 1, type: 'Redes Sociais', status: 'INACTIVE', created_at: '2025-06-01', updated_at: '2025-08-01' },
+            {
+                id: 1,
+                title: 'Titulo do Questionario 01',
+                project_id: 1,
+                is_randomized_answers: true,
+                filter_id: null,
+                status: 'active',
+                created_at: '2025-06-01',
+                updated_at: '2025-08-01'
+            },
         ];
     }
 }
