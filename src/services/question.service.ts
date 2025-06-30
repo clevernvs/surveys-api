af1b67e98c2f02428aaa27438a40182798390c71
Cleverton Neves <clever.nvs@gmail.com>
2025-06-30 17:54:05 -0300

    refact: Refatorando a service de Perguntas

--- src/services/question.service.ts
+++ src/services/question.service.ts
@@ -4,31 +4,42 @@ export class QuestionService {
             {
                 id: 1,
                 title: 'Titulo da Pergunta 01',
-                question_type_id: 2,
-                is_randomized_answers: false,
-                filter_id: 1,
+                question_type_id: 1,
+                questionnaire_id: 1,
+                is_required: true,
+                media: {
+                    video: null,
+                    image: null,
+                },
                 created_at: '2025-06-01',
                 updated_at: '2025-06-30',
             },
             {
-                id: 2,
+                id: 1,
                 title: 'Titulo da Pergunta 02',
-                question_type_id: 1,
-                is_randomized_answers: true,
-                filter_id: 2,
+                question_type_id: 2,
+                questionnaire_id: 1,
+                is_required: false,
+                media: {
+                    video: null,
+                    image: null,
+                },
                 created_at: '2025-06-01',
                 updated_at: '2025-06-30',
             },
             {
-                id: 3,
+                id: 1,
                 title: 'Titulo da Pergunta 03',
-                question_type_id: 1,
-                is_randomized_answers: false,
-                filter_id: null,
+                question_type_id: 5,
+                questionnaire_id: 1,
+                is_required: false,
+                media: {
+                    video: null,
+                    image: null,
+                },
                 created_at: '2025-06-01',
                 updated_at: '2025-06-30',
             },
-
         ];
     }
 }
