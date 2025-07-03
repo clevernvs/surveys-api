9e43dd1aa9dbccb6447d50f8b80efee238828eb1
Cleverton Neves <clever.nvs@gmail.com>
2025-07-03 16:31:42 -0300

    fix: Corrigindo o endpoint de Get All Answers

--- src/services/answer.service.ts
+++ src/services/answer.service.ts
@@ -1,47 +1,13 @@
+import prisma from '../prisma/client';
+
 export class AnswerService {
     async findAll() {
-        return [
-            {
-                id: 1,
-                title: 'Titulo da Resposta 01',
-                question_id: 1,
-                numeric_order: 3,
-                is_fixed: true,
-                jump_to_question_id: 1,
-                created_at: '2025-06-01',
-                updated_at: '2025-06-30',
-            },
-            {
-                id: 2,
-                title: 'Titulo da Resposta 02',
-                question_type_id: 1,
-                numeric_order: 4,
-                is_fixed: false,
-                jump_to_question_id: null,
-                created_at: '2025-06-01',
-                updated_at: '2025-06-30',
-            },
-            {
-                id: 3,
-                title: 'Titulo da Resposta 03',
-                question_type_id: 1,
-                numeric_order: 2,
-                is_fixed: false,
-                jump_to_question_id: null,
-                created_at: '2025-06-01',
-                updated_at: '2025-06-30',
-            },
-            {
-                id: 4,
-                title: 'Titulo da Resposta 04',
-                question_type_id: 1,
-                numeric_order: 1,
-                is_fixed: false,
-                jump_to_question_id: null,
-                created_at: '2025-06-01',
-                updated_at: '2025-06-30',
-            },
-
-        ];
+        try {
+            const answers = await prisma.answer.findMany();
+            return answers;
+        } catch (error) {
+            console.error('Erro ao buscar respostas:', error);
+            throw new Error('Erro ao buscar respostas no banco de dados');
+        }
     }
 }
